759cf2dba7c329650dcf1c20557ddf9483d3c368
EstherOA <estherowusuansah19@gmail.com>
2025-09-22 10:51:29 +0000

    remove all assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,9 +8,14 @@ module.exports = {
         output: ["html", "json"],
       },
     },
-    // assert: {
-    //   preset: "lighthouse:recommended",
-    // },
+    assert: {
+      // "preset": "lighthouse:recommended",
+      // "assertions": {
+      //   "categories:performance": ["error", { "minScore": 0.9 }],
+      //   "categories:accessibility": ["warn", { "minScore": 0.95 }]
+      // }
+      assertions: {},
+    },
   },
   upload: {
     target: "filesystem",
