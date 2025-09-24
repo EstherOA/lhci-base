9711cf834d5b356a408bac78cb261b98a0e78877
EstherOA <estherowusuansah19@gmail.com>
2025-09-24 14:07:46 +0000

    use commonjs

--- scripts/build-lhci-urls.js
+++ scripts/build-lhci-urls.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 const appDir = path.join(process.cwd(), "src/app"); //Use pages if using Pages Router
 
